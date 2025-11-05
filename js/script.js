6d6a3cfcc8b7bcc919254fe4baaa9f6440fda638
jbAfpa <jea.vau@dwwm.afpa>
2025-11-05 16:21:03 +0100

    js test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
 
+// Appeler les JSON pour remplir les sections dynamiquement    
+
+// TODO :  id / title / vote_average / relese_date / poster_path / backdrop_path
+
+// Carouselle des sorties de la semaine
+
+const URL =  `./json/data.json`;
+
+fetch(URL)
+    .then(result => {
+        if (!result.ok) throw new Error("oups, erreur initialisataion details.json");
+        return result.json();
+    })
+    .then(movie => {
+        console.log(movie);
+
+    })
+    .catch(error => {
+        console.error("Erreur lors du chargement du fichier JSON :", error);
+    });
