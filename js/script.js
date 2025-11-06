fc76fbcc1afca69dc70ebc5aa8df09e3bfd2fff3
Jb Maison <jb.vaugi@hotmail.fr>
2025-11-06 14:45:40 +0100

    Génération cartes ok

--- js/script.js
+++ js/script.js
@@ -1,80 +1,111 @@
 "use strict";
 
+// Appeler les JSON pour remplir les sections dynamiquement    
+
+
 // Configuration des URLs d'images
 const CONFIG = {
     BASE_IMG_URL: 'https://image.tmdb.org/t/p/',
     POSTER_SIZE: 'w500',
 };
 
-
-// Appeler les JSON pour remplir les sections dynamiquement    
-
-// TODO :  id / title / vote_average / relese_date / poster_path / backdrop_path
-
-// Carouselle des sorties de la semaine
-
 const URL =  `./json/data.json`;
 
 
-fetch(URL)
-    .then(result => {
-        if (!result.ok) throw new Error("oups, erreur initialisataion details.json");
-        return result.json();
-    })
-    .then(movie => {
-        console.log(movie);
+    // GESTION CAROUSEL SORTIES DE LA SEMAINE
+    fetch(URL)
+        .then(result => {
+            if (!result.ok) throw new Error("oups, erreur carouselle initialisataion details.json");
+            return result.json();
+        })
+        .then(movie => {
+            console.log(movie);
 
-        // recupération des éléments html
+            // recupération des éléments html
 
-        let fondCarou = document.getElementById('backgroundCarou');
-        let affiche = document.getElementById('posterSortie');
-        let titre = document.getElementById('titreS');
-        let dateS = document.getElementById('release-date');
-        let note = document.getElementById('idSortieRating');
+            let fondCarou = document.getElementById('backgroundCarou');
+            let affiche = document.getElementById('posterSortie');
+            let titre = document.getElementById('titreS');
+            let dateS = document.getElementById('release-date');
+            let note = document.getElementById('idSortieRating');
 
 
-        // remplisage carousel 01
-        fondCarou.src = CONFIG.BASE_IMG_URL + 'original' + movie.results[0].backdrop_path;
-        affiche.src = CONFIG.BASE_IMG_URL + CONFIG.POSTER_SIZE + movie.results[0].poster_path;
-        titre.innerText = movie.results[0].title;
-        dateS.innerText = formatDate(movie.results[0].release_date);
-        note.innerText = movie.results[0].vote_average;
+            // remplisage carousel 01
+            fondCarou.src = CONFIG.BASE_IMG_URL + 'original' + movie.results[0].backdrop_path;
+            affiche.src = CONFIG.BASE_IMG_URL + CONFIG.POSTER_SIZE + movie.results[0].poster_path;
+            titre.innerText = movie.results[0].title;
+            dateS.innerText = formatDate(movie.results[0].release_date);
+            note.innerText = movie.results[0].vote_average;
 
-        // Remplir les deux autres carousels.
-        
-        for (let i = 1; i < 3; i++) {       // 2  films Suivant.
-        const FILM = movie.results[i];
-        let carouSuite = document.querySelector('.carousel-inner');
+            // Remplir les deux autres carousels.
 
-        carouSuite.innerHTML += `
-        <div class="carousel-item c-item" data-bs-interval="2000">
-            <img src="${CONFIG.BASE_IMG_URL}original${FILM.backdrop_path}" class="d-block w-100 c-img" alt="${FILM.title}">
-            <div class="carousel-caption d-none d-md-block">    
-                <div class="container container_sortie" id="sortie-section">
-                    <img src="${CONFIG.BASE_IMG_URL}${CONFIG.POSTER_SIZE}${FILM.poster_path}"
-                            id="posterSortie" alt="Affiche de ${FILM.title}">
-                    <div class="sortie-info">
-                        <h2 id="titreS">${FILM.title}</h2>  
-                        <p><strong>Date de sortie :</strong> <span id="release-date">
-                            ${formatDate(FILM.release_date)}</span></p>
-                        <p class="sortieRating">⭐ <span id="idSortieRating">
-                            ${FILM.vote_average.toFixed(1)}</span> / 10</p>
+            for (let i = 1; i < 3; i++) {       // 2  films Suivant.
+            const FILM = movie.results[i];
+            let carouSuite = document.querySelector('.carousel-inner');
+
+            carouSuite.innerHTML += `
+            <div class="carousel-item c-item" data-bs-interval="2000">
+                <img src="${CONFIG.BASE_IMG_URL}original${FILM.backdrop_path}" class="d-block w-100 c-img" alt="${FILM.title}">
+                <div class="carousel-caption d-none d-md-block">    
+                    <div class="container container_sortie" id="sortie-section">
+                        <img src="${CONFIG.BASE_IMG_URL}${CONFIG.POSTER_SIZE}${FILM.poster_path}"
+                                id="posterSortie" alt="Affiche de ${FILM.title}">
+                        <div class="sortie-info">
+                            <h2 id="titreS">${FILM.title}</h2>  
+                            <p><strong>Date de sortie :</strong> <span id="release-date">
+                                ${formatDate(FILM.release_date)}</span></p>
+                            <p class="sortieRating">⭐ <span id="idSortieRating">
+                                ${FILM.vote_average.toFixed(1)}</span> / 10</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        `;
-        }
+            `;
+            }
 
 
-    })
+        })
 
-    .catch(error => {
-        console.error("Erreur lors de la récupération des données :", error);
-    });
+        .catch(error => {
+            console.error("Erreur lors de la récupération des données du carouselles :", error);
+        });
+
+    // GESTION CARTE ANIMES
+    fetch(URL)
+        .then(result => {
+            if (!result.ok) throw new Error("oups, erreur cartes initialisataion details.json");
+            return result.json();
+        })
+        .then(movie => {
+            console.log(movie);
+
+            for (let i = 0; i < 12; i++) {       // 10 films Suivant.
+            const FILM = movie.results[i];
+            let carteGenre = document.querySelector('.film-cards-container');
+
+            carteGenre.innerHTML += ` <div class="film-card">
+                    <img src="${CONFIG.BASE_IMG_URL}${CONFIG.POSTER_SIZE}${FILM.poster_path}" class="film-poster" alt="Poster de ${FILM.title}">
+                    <div class="film-info">
+                        <h3 class="film-title">${FILM.title}</h3>
+                        <p class="film-date">Date de sortie : <span>${formatDate(FILM.release_date)}</span></p>
+                        <p class="film-rating">⭐ <span>${FILM.vote_average.toFixed(1)}</span>/10</p>
+                    </div>
+                </div>`;
+        
+            }
+
+        })
+
+        .catch(error => {
+            console.error("Erreur lors de la récupération des données du cartes :", error);
+        });
 
 
-    // Fonctions utilitaires
+
+
+
+// ------------------------------------ Fonctions Utilitaires ------------------------------------
+
     const formatDate = (dateStr) => {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         return new Date(dateStr).toLocaleDateString('fr-FR', options);
